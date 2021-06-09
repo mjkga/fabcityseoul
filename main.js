348b164a86f296ce0b49c72173d6c49e33c96a85
kezi <you@example.com>
2021-06-09 23:47:21 +0900

    github webhosting test

--- main.js
+++ main.js
@@ -15,22 +15,6 @@ app.set('views','./files')
 app.set('view engine','ejs');
 app.engine('html', require('ejs').renderFile);
 
-// routing
-app.get('/', function (req, res) {
-    res.render("index.html");
-});
-app.get('/origin', function (req, res) {
-    res.render("index_origin.html");
-});
-app.get('/cases', function (req, res) {
-    res.render("cases.html");
-});
-app.get('/e', function (req, res) {
-    errorPageReturn(res);
-});
-
-
-
 // open port
 app.listen( process.env.WEB_SERVER_PORT, function(){
     console.log(
